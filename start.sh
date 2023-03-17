c624e16af789a8a3ade266bd0cbaaf885e7270de
WARRIORS <56763972+ccadmin1@users.noreply.github.com>
2023-03-17 14:38:09 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/Greymattersbot/EvaMaria /EvaMaria
+  git clone https://github.com/ccadmin1/eva5
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /EvaMaria
+  git clone $UPSTREAM_REPO /eva5
 fi
-cd /EvaMaria
+cd /eva5
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
